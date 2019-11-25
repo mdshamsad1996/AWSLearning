03d146b48cdada53b8ff067ffddfae9c6f899dbe
shamsadgrokr <shamsad.alam@datagrokr.com>
2019-11-25 23:02:57 +0530

    WIP: Added Autoscaling Template and python file to exhaust CPU Utiliztaion

---
+++ autoScaling-ec2/cpuload.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""
+Produces load on all available CPU cores
+"""
+
+from multiprocessing import Pool
+from multiprocessing import cpu_count
+
+def f(x):
+    while True:
+        x*x
+
+if __name__ == '__main__':
+    processes = cpu_count()
+    pool = Pool(processes)
+    pool.map(f, range(processes))
